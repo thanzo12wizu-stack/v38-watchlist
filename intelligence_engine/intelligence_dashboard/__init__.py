41555e73eb989f133c6443b33adc67740f31087a
thanzo12wizu-stack <thanzo.12.wiz.u@gmail.com>
2026-07-23 10:45:28 +0900

    Use mobile dashboard runtime

--- intelligence_engine/intelligence_dashboard/__init__.py
+++ intelligence_engine/intelligence_dashboard/__init__.py
@@ -1,4 +1,4 @@
-from .app import build_html, generate, main
 from .formatting import load_payload
+from .runtime import build_html, generate, main
 
 __all__ = ["build_html", "generate", "load_payload", "main"]
