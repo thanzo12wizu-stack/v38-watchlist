bd58e069946763bf06be148eb78b4ee3620e066b
thanzo12wizu-stack <thanzo.12.wiz.u@gmail.com>
2026-07-28 07:14:32 +0900

    Use multipage journal navigation

--- intelligence_engine/trade_journal_render.py
+++ intelligence_engine/trade_journal_render.py
@@ -1,4 +1,4 @@
-from .trade_journal_tabs import render_dashboard
+from .trade_journal_multipage import render_dashboard
 from .trade_journal_cards import render_daily_card, render_portfolio_card, write_social_copy
 
 __all__ = ["render_dashboard", "render_daily_card", "render_portfolio_card", "write_social_copy"]
